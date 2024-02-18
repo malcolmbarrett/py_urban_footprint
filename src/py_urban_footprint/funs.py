72ffbf7709a72868598b0ee6b29d32d7aeee3120
Malcolm Barrett <malcolmbarrett@gmail.com>
2024-02-18 13:39:24 -0500

    add docstring

--- src/py_urban_footprint/funs.py
+++ src/py_urban_footprint/funs.py
@@ -1,6 +1,15 @@
 
 
 def gt_summarize_harvest(df): 
+    """
+    Summarizes the harvest data by FarmType in the given DataFrame.
+
+    Parameters:
+    df (polars.DataFrame): The input DataFrame containing the harvest data.
+
+    Returns:
+    great_tables.GT: The summarized harvest data in a great_tables.GT object.
+    """
     import polars as pl
     from great_tables import GT
     harvest = (df
