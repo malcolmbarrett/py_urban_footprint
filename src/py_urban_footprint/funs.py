21cc627454f6ce0d7048225d86be1e74081617d8
Malcolm Barrett <malcolmbarrett@gmail.com>
2024-02-18 15:20:17 -0500

    fmt

--- src/py_urban_footprint/funs.py
+++ src/py_urban_footprint/funs.py
@@ -1,4 +1,9 @@
-def gt_summarize_harvest(df): 
+import polars as pl
+from great_tables import GT
+import seaborn as sns
+
+
+def gt_summarize_harvest(df):
     """
     Summarizes the harvest data by FarmType in the given DataFrame.
 
@@ -8,18 +13,16 @@ def gt_summarize_harvest(df):
     Returns:
     great_tables.GT: The summarized harvest data in a great_tables.GT object.
     """
-    import polars as pl
-    from great_tables import GT
-    harvest = (df
-        .group_by("FarmType")
-        .agg(pl.col("Harvested").mean())
-        .sort("Harvested"))
+    harvest = df.group_by("FarmType").agg(pl.col("Harvested").mean()).sort("Harvested")
 
-    gt = (GT(harvest)
+    gt = (
+        GT(harvest)
         .fmt_number(columns=["Harvested"])
-        .cols_label(FarmType="Farm Type", Harvested="Harvested (kg)"))
+        .cols_label(FarmType="Farm Type", Harvested="Harvested (kg)")
+    )
     return gt
-    
+
+
 def gt_summarize_water_by_solar(farms):
     """
     Summarizes water usage by solar panels.
@@ -30,18 +33,17 @@ def gt_summarize_water_by_solar(farms):
     Returns:
         great_tables.GT: A formatted table summarizing water usage by solar panels.
     """
-    import polars as pl
-    from great_tables import GT
+    water_use_by_solar = (
+        farms.filter(pl.col("SolarPanels_bin") != "")
+        .group_by(pl.col("SolarPanels_bin"))
+        .agg(pl.col("WaterUse_GWP_2019").mean().alias("water_use"), pl.len().alias("n"))
+        .sort("water_use")
+    )
 
-    water_use_by_solar = farms.filter(pl.col("SolarPanels_bin") != "").group_by(pl.col("SolarPanels_bin")).agg(
-        pl.col("WaterUse_GWP_2019").mean().alias("water_use"),
-        pl.len().alias("n")
-    ).sort("water_use")
-
-    tbl = GT(water_use_by_solar).fmt_number("water_use").cols_label(
-        SolarPanels_bin="Solar Panels",
-        water_use="Water Use (L)",
-        n="N"
+    tbl = (
+        GT(water_use_by_solar)
+        .fmt_number("water_use")
+        .cols_label(SolarPanels_bin="Solar Panels", water_use="Water Use (L)", n="N")
     )
 
     return tbl
@@ -57,15 +59,9 @@ def plot_water_usage(farms):
     Returns:
         a seaborn plot
     """
-    import seaborn as sns
-    import polars as pl
-    plot_data = (
-        farms
-            .filter(pl.col("SolarPanels_bin") != "")
-            .with_columns(
-                pl.col("SizeOfProductionSite").log(),
-                pl.col("WaterUse_GWP_2019").log(),
-            )
+    plot_data = farms.filter(pl.col("SolarPanels_bin") != "").with_columns(
+        pl.col("SizeOfProductionSite").log(),
+        pl.col("WaterUse_GWP_2019").log(),
     )
 
     plot = sns.lmplot(
@@ -75,7 +71,7 @@ def plot_water_usage(farms):
         hue="FarmType",
         col="FarmType",
         palette="colorblind",
-        legend="full"
+        legend="full",
     )
 
     return plot
