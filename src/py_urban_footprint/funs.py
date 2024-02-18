d19418fb8065ba3578b20709a9dcb001fdc7ac24
Malcolm Barrett <malcolmbarrett@gmail.com>
2024-02-18 15:03:57 -0500

    analyze farms a bit

--- src/py_urban_footprint/funs.py
+++ src/py_urban_footprint/funs.py
@@ -16,6 +16,7 @@ def gt_summarize_harvest(df):
         .group_by("FarmType")
         .agg(pl.col("Harvested").mean())
         .sort("Harvested"))
+        
     gt = (GT(harvest)
         .fmt_number(columns=["Harvested"])
         .cols_label(FarmType="Farm Type", Harvested="Harvested (kg)"))
